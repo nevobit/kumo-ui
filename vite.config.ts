f4ce80aae6f0d2ff008c076bec8878c197fd0aae
Nestor Mosquera <yunsde18@gmail.com>
2023-10-01 20:36:09 -0500

    chore update build css

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path';
-import VitePluginCSS from 'vite-plugin-css'; // Agrega esta línea
+import css from 'vite-plugin-css'; // Agrega esta línea
 import tsConfigPaths from 'vite-tsconfig-paths';
 import dts from 'vite-plugin-dts';
-import vitePluginCssModules from "vite-plugin-css-modules";
-import loadCssModulePlugin from 'vite-plugin-load-css-module'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+
 // @ts-ignore
 
 import * as packageJson from "./package.json";
-
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    cssInjectedByJsPlugin({ relativeCSSInjection: true }),
+    tsConfigPaths(),
+    dts({
+      include: ['src/**/*', 'src/**/*.css'],
+      exclude: ['src/**/*.stories.tsx', 'src/storybook/**/*'],
+    }),
+  
   ],
-
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+  },
   css: {
     modules: {
       scopeBehaviour: 'global'
-    }
+    },
+    postcss: {},
+    preprocessorOptions: {
+    },
   },
 
-
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: "src/index.ts",
+      output: {
+        entryFileNames: "index.js",
+      }
+    }
+  }
 })
